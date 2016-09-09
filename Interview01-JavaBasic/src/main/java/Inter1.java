8619d911258341c8663626370309377710b85838
codenewbie <18267853511@163.com>
2016-09-09 11:23:37 +0800

    Inter1-关于i++和++i 建立

---
+++ Interview01-JavaBasic/src/main/java/Inter1.java
@@ -0,0 +1,17 @@
+/**
+ * 关于i++和++i
+ * @auther mynawang
+ * @create 2016-09-09 11:13
+ */
+public class Inter1 {
+
+    public static void main(String[] args) {
+        int i = 1;
+        System.out.println("i+++i++: " + (i+++i++));
+        System.out.println("i: " + (i));
+        System.out.println("i+++++i: " + ((i++)+(++i)));
+        System.out.println("i: " + (i));
+        System.out.println("i+++i+++i++: " + (i+++i+++i++));
+        System.out.println("i: " + (i));
+    }
+}
